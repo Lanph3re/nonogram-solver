33457ba09db2c20ab3646544983e37177432fafb
Lanph3re <lanph3re@gmail.com>
2020-02-23 15:33:54 +0900

    add new algorithm - glue

--- src/solver.py
+++ src/solver.py
@@ -66,7 +66,79 @@ def simple_end(nonogram):
             board[row_idx][-1 - last_clue] = nonogram.SPACE
 
 
+def get_firstbox_column(nonogram, col_idx):
+    for i in range(nonogram.col_size):
+        if nonogram.board[i][col_idx] == nonogram.BOX:
+            return i
+    return None
+
+
+def get_lastbox_column(nonogram, col_idx):
+    for i in range(nonogram.col_size):
+        if nonogram.board[-(i + 1)][col_idx] == nonogram.BOX:
+            return -(i + 1)
+    return None
+
+
+def get_firstbox_row(nonogram, row_idx):
+    for i in range(nonogram.row_size):
+        if nonogram.board[row_idx][i] == nonogram.BOX:
+            return i
+    return None
+
+
+def get_lastbox_column(nonogram, row_idx):
+    for i in range(nonogram.row_size):
+        if nonogram.board[row_idx][-(i + 1)] == nonogram.BOX:
+            return -(i + 1)
+    return None
+
+
+def glue(nonogram):
+    board = nonogram.board
+    # columns
+    for col_idx in range(nonogram.num_col):
+        first_box = get_firstbox_column(nonogram, col_idx)
+        if first_box is not None:
+            distance = first_box + 1
+            first_clue = nonogram.col_clues[col_idx][0]
+            if distance < first_clue:
+                for i in range(first_clue - distance):
+                    if first_box + 1 + i < nonogram.col_size:
+                        board[first_box + 1 + i][col_idx] = nonogram.BOX
+
+        last_box = get_lastbox_column(nonogram, col_idx)
+        if last_box is not None:
+            distance = -last_box
+            last_clue = nonogram.col_clues[col_idx][-1]
+            if distance < last_clue:
+                for i in range(last_clue - distance):
+                    if last_box + 1 + i < nonogram.col_size:
+                        board[last_box - 1 - i][col_idx] = nonogram.BOX
+
+    # rows
+    for row_idx in range(nonogram.num_row):
+        first_box = get_firstbox_row(nonogram, row_idx)
+        if first_box is not None:
+            distance = first_box + 1
+            first_clue = nonogram.row_clues[row_idx][0]
+            if distance < first_clue:
+                for i in range(first_clue - distance):
+                    if first_box + 1 + i < nonogram.col_size:
+                        board[row_idx][first_box + 1 + i] = nonogram.BOX
+
+        last_box = get_lastbox_column(nonogram, row_idx)
+        if last_box is not None:
+            distance = -last_box
+            last_clue = nonogram.row_clues[row_idx][-1]
+            if distance < last_clue:
+                for i in range(last_clue - distance):
+                    if last_box + 1 + i < nonogram.col_size:
+                        board[row_idx][last_box - 1 - i] = nonogram.BOX
+
+
 def solver(nonogram):
-    simple_boxes(nonogram)
-    for _ in range(2):
+    for _ in range(5):
+        simple_boxes(nonogram)
         simple_end(nonogram)
+        glue(nonogram)
